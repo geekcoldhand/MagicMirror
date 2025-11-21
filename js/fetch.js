ffb94dd2e50e4d87637e12eba6406f1945cf7fa6
geekcoldhand <horatiousaharris@gmail.com>
2025-11-21 09:04:11 -0500

    new fetch wrappper

--- js/fetch.js
+++ js/fetch.js
@@ -1,27 +1,31 @@
-/**
- * Helper class to provide either third party fetch library or (if node >= 18)
- * return internal node fetch implementation.
- *
- * Attention: After some discussion we always return the third party
- * implementation until the node implementation is stable and more tested
- * @see https://github.com/MichMich/MagicMirror/pull/2952
- * @see https://github.com/MichMich/MagicMirror/issues/2649
- * @param {string} url to be fetched
- * @param {object} options object e.g. for headers
- * @class
- */
-async function fetch(url, options = {}) {
-	// const nodeVersion = process.version.match(/^v(\d+)\.*/)[1];
-	// if (nodeVersion >= 18) {
-	// 	// node version >= 18
-	// 	return global.fetch(url, options);
-	// } else {
-	// 	// node version < 18
-	// 	const nodefetch = require("node-fetch");
-	// 	return nodefetch(url, options);
-	// }
-	const nodefetch = require("node-fetch");
-	return nodefetch(url, options);
+const fetch = require("node-fetch");
+const Log = require("logger");
+
+class Fetcher {
+  constructor() {
+    this.fetchTimeout = 5000;
+  }
+
+  async fetch(url, options = {}) {
+    const timeout = options.timeout || this.fetchTimeout;
+    
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), timeout);
+
+    try {
+      const response = await fetch(url, {
+        ...options,
+        signal: controller.signal
+      });
+      
+      clearTimeout(timeoutId);
+      return response;
+    } catch (error) {
+      clearTimeout(timeoutId);
+      Log.error("Fetch error:", error);
+      throw error;
+    }
+  }
 }
 
-module.exports = fetch;
+module.exports = new Fetcher();
