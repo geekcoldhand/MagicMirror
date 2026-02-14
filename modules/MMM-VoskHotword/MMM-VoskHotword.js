e0eabc8478bcd2a05e6e049edc33cb71ee4540c5
geekcoldhand <horatiousaharris@gmail.com>
2026-02-14 12:57:03 -0500

    tweaking the hotword js module for more logging

--- modules/MMM-VoskHotword/MMM-VoskHotword.js
+++ modules/MMM-VoskHotword/MMM-VoskHotword.js
@@ -5,20 +5,30 @@ Module.register("MMM-VoskHotword", {
 
 	start: function () {
 		Log.info("MMM-VoskHotword: External Pi 2 hotword detection mode");
+		Log.info("MMM-VoskHotword: Ready to receive socket notifications");
 	},
 
 	socketNotificationReceived: function (notification, payload) {
+		Log.info("MMM-VoskHotword: Received socket notification:", notification);
+
 		if (notification === "HOTWORD_DETECTED") {
 			Log.info("🎯 Hotword detected:", payload.hotword, "from", payload.source);
+			Log.info("Broadcasting HOTWORD_DETECTED to all modules...");
 
-			// Trigger Google Assistant
+			// Broadcast to all modules FIRST
+			this.sendNotification("HOTWORD_DETECTED", payload);
+			Log.info("✓ Broadcast sent");
+
+			// Then trigger Assistant
 			this.sendNotification("ASSISTANT_ACTIVATE", {
 				profile: this.config.assistantProfile
 			});
-
-			// trigger hotword event
-			this.sendNotification("HOTWORD_DETECTED", payload);
+			Log.info("✓ ASSISTANT_ACTIVATE sent");
 		}
+	},
+
+	notificationReceived: function (notification, payload, sender) {
+		Log.info("MMM-VoskHotword received notification:", notification, "from", sender ? sender.name : "system");
 	},
 
 	getDom: function () {
